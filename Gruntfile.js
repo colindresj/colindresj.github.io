f843f10ad114b2ef13ea83169f1bd7e96dea8554
JC <jorge.e.colindres@gmail.com>
2014-01-12 18:31:55 -0500

    finished building the gruntfile. default task includes coffe compilation, uglify and concanate bower_component sources with main.js to produce single main.min.js, scss compilation, copy bower_component css into assets, css minify and join all css in to main.min.css, jekyll build. Also created a grunt serve alias for jekyll serve --watch

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    coffee: {
+      compile: {
+        files: {
+          'assets/js/main.js': 'assets/coffee/main.coffee'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        report: 'gzip'
+      },
+      dist: {
+        files: {
+          'assets/js/main.min.js': ['bower_components/fastclick/lib/fastclick.js', 'bower_components/swipebox/source/jquery.swipebox.js', 'assets/js/main.js']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'assets/css/main.css': 'assets/scss/main.scss'
+        }
+      }
+    },
+    copy: {
+      swipebox: {
+        files: [
+          {
+            src: 'bower_components/swipebox/source/swipebox.css',
+            dest: 'assets/css/swipebox.css'
+          },
+          {
+            expand: true,
+            cwd: 'bower_components/swipebox/source/img/',
+            src: ['**'],
+            dest: 'assets/css/img/'
+          }
+        ]
+      }
+    },
+    cssmin: {
+      options: {
+        report: 'gzip'
+      },
+      combine: {
+        files: {
+          'assets/css/main.min.css': ['assets/css/main.css', 'assets/css/swipebox.css']
+        }
+      }
+    },
+    exec: {
+      build: {
+        cmd: 'jekyll build'
+      },
+      serve: {
+        cmd: 'jekyll serve --watch'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('default', [ 'coffee', 'uglify', 'sass', 'copy', 'cssmin', 'exec:build' ]);
+  grunt.registerTask('serve', ['exec:serve']);
+
+};
+
